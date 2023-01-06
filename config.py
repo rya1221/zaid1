a43ce8701ec026c3df330e9d08a79f5d4a3e8635
heaven805 <84675556+heaven805@users.noreply.github.com>
2023-01-06 17:26:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,14 +10,14 @@ API_ID = int(getenv("API_ID", "6435225")) #optional
 API_HASH = getenv("API_HASH", "") #optional
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
-OWNER_ID = int(getenv("OWNER_ID"))
-MONGO_URL = getenv("MONGO_URL")
+OWNER_ID = int(getenv("OWNER_ID",""))
+MONGO_URL = getenv("MONGO_URL","")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
 ALIVE_PIC = getenv("ALIVE_PIC")
 ALIVE_TEXT = getenv("ALIVE_TEXT")
-PM_LOGGER = getenv("PM_LOGGER")
-LOG_GROUP = getenv("LOG_GROUP")
-GIT_TOKEN = getenv("GIT_TOKEN") #personal access token
+PM_LOGGER = getenv("PM_LOGGER","")
+LOG_GROUP = getenv("LOG_GROUP","")
+GIT_TOKEN = getenv("GIT_TOKEN","") #personal access token
 REPO_URL = getenv("REPO_URL", "https://github.com/ITZ-ZAID/ZAID-USERBOT")
 BRANCH = getenv("BRANCH", "master") #don't change
  
